e3c22d324c80ce45e839a64e043c6472d542d5ee
Adem Özsayın <ozsayinadem@gmail.com>
2024-06-21 20:45:36 +0300

    Add Env

---
+++ Packages/Env/Sources/Env/RouterPath.swift
@@ -0,0 +1,85 @@
+import Combine
+import Foundation
+import Network
+import Observation
+import SwiftUI
+import YouTubeKit
+
+public enum RouterDestination: Hashable {
+   
+    case mutedAccounts
+    case usersPlaylists(playlists: [YTPlaylist])
+    case playlistDetails(playlist: YTPlaylist)
+}
+
+public enum WindowDestinationEditor: Hashable, Codable {
+    case quoteLinkStatusEditor(link: URL)
+}
+
+public enum WindowDestinationMedia: Hashable, Codable {
+//    case mediaViewer(attachments: [MediaAttachment], selectedAttachment: MediaAttachment)
+    case mediaViewer
+}
+
+public enum SheetDestination: Identifiable, Hashable {
+    public static func == (lhs: SheetDestination, rhs: SheetDestination) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+ 
+    case settings
+    case about
+    case miniPlayer
+ 
+    
+    public var id: String {
+        switch self {
+            case .settings, .about:
+                "settings"
+            case .miniPlayer:
+                "miniPlayer"
+        }
+    }
+}
+
+public enum SettingsStartingPoint {
+    case display
+    case haptic
+    case remoteTimelines
+    case tagGroups
+    case recentTags
+    case content
+    case swipeActions
+    case tabAndSidebarEntries
+    case translation
+}
+
+@MainActor
+@Observable public class RouterPath {
+//    public var client: Client?
+    public var urlHandler: ((URL) -> OpenURLAction.Result)?
+    
+    public var path: [RouterDestination] = []
+    public var presentedSheet: SheetDestination?
+    
+    public static var settingsStartingPoint: SettingsStartingPoint? = nil
+    
+    public init() {}
+    
+    public func navigate(to: RouterDestination) {
+        path.append(to)
+    }
+    
+    private func handlerOrDefault(url: URL) {
+        if let urlHandler {
+            _ = urlHandler(url)
+        } else {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    
+}
---
+++ Packages/Env/Sources/Env/UserPreferences.swift
@@ -0,0 +1,140 @@
+import Combine
+import Foundation
+import SwiftUI
+
+@MainActor
+@Observable public class UserPreferences: Sendable {
+    final class Storage {
+        @AppStorage("preferred_browser") public var preferredBrowser: PreferredBrowser = .inAppSafari
+                
+        @AppStorage("haptic_tab") public var hapticTabSelectionEnabled = true
+        @AppStorage("haptic_timeline") public var hapticTimelineEnabled = true
+        @AppStorage("haptic_button_press") public var hapticButtonPressEnabled = true
+        @AppStorage("sound_effect_enabled") public var soundEffectEnabled = true
+        
+        @AppStorage("show_second_column_ipad") public var showiPadSecondaryColumn = true
+        @AppStorage("show_tab_label_iphone") public var showiPhoneTabLabel = true
+
+        @AppStorage("inAppBrowserReaderView") public var inAppBrowserReaderView = false
+        @AppStorage("enableAutoPlayAtStart") public var enableAutoPlayAtStart = true
+
+        @AppStorage("showBackgroundImage") public var showBackgroundImage = true
+       
+        @AppStorage("sidebar_expanded") public var isSidebarExpanded: Bool = false
+
+        init() {
+            prepareTranslationType()
+        }
+        
+        private func prepareTranslationType() {
+            let sharedDefault = UserDefaults.standard
+        }
+    }
+    
+    public static let sharedDefault = UserDefaults(suiteName: "group.agency.fiable.OnlyJose")
+    public static let shared = UserPreferences()
+    private let storage = Storage()
+    
+    public var preferredBrowser: PreferredBrowser {
+        didSet {
+            storage.preferredBrowser = preferredBrowser
+        }
+    }
+    
+    public var hapticTabSelectionEnabled: Bool {
+        didSet {
+            storage.hapticTabSelectionEnabled = hapticTabSelectionEnabled
+        }
+    }
+    
+    public var hapticTimelineEnabled: Bool {
+        didSet {
+            storage.hapticTimelineEnabled = hapticTimelineEnabled
+        }
+    }
+    
+    public var hapticButtonPressEnabled: Bool {
+        didSet {
+            storage.hapticButtonPressEnabled = hapticButtonPressEnabled
+        }
+    }
+    
+    public var soundEffectEnabled: Bool {
+        didSet {
+            storage.soundEffectEnabled = soundEffectEnabled
+        }
+    }
+    
+    public var showiPadSecondaryColumn: Bool {
+        didSet {
+            storage.showiPadSecondaryColumn = showiPadSecondaryColumn
+        }
+    }
+    
+    
+    public var showiPhoneTabLabel: Bool {
+        didSet {
+            storage.showiPhoneTabLabel = showiPhoneTabLabel
+        }
+    }
+    
+    public var inAppBrowserReaderView: Bool {
+        didSet {
+            storage.inAppBrowserReaderView = inAppBrowserReaderView
+        }
+    }
+    
+    public var enableAutoPlayAtStart: Bool {
+        didSet {
+            storage.enableAutoPlayAtStart = enableAutoPlayAtStart
+        }
+    }
+    
+    public var showBackgroundImage: Bool {
+        didSet {
+            storage.showBackgroundImage = showBackgroundImage
+        }
+    }
+    
+    public var isSidebarExpanded: Bool {
+        didSet {
+            storage.isSidebarExpanded = isSidebarExpanded
+        }
+    }
+
+    
+    private init() {
+        preferredBrowser = storage.preferredBrowser
+ 
+        hapticTabSelectionEnabled = storage.hapticTabSelectionEnabled
+        hapticTimelineEnabled = storage.hapticTimelineEnabled
+        hapticButtonPressEnabled = storage.hapticButtonPressEnabled
+        
+        soundEffectEnabled = storage.soundEffectEnabled
+        showiPadSecondaryColumn = storage.showiPadSecondaryColumn
+    
+        showiPhoneTabLabel = storage.showiPhoneTabLabel
+        
+        inAppBrowserReaderView = storage.inAppBrowserReaderView
+        enableAutoPlayAtStart = storage.enableAutoPlayAtStart
+        
+        showBackgroundImage = storage.showBackgroundImage
+        
+        isSidebarExpanded = storage.isSidebarExpanded
+
+    }
+}
+
+extension UInt: RawRepresentable {
+    public var rawValue: Int {
+        Int(self)
+    }
+    
+    public init?(rawValue: Int) {
+        if rawValue >= 0 {
+            self.init(rawValue)
+        } else {
+            return nil
+        }
+    }
+}
